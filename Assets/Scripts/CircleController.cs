47e2a38fa7e36b0a5391a5ea820123b043aa259b
CyberFries <2376511917@qq.com>
2025-02-04 23:07:10 +0800

    Unity Files

--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -12,12 +12,11 @@ public class CircleController : MonoBehaviour
     public bool radiusConnection; //是否启用L和R
     public float rotateL; //也是角度制
     public float rotateR; //也是角度制
+    public float isConvex; //true生成凸多边形，false生成凹多边形
 
-    private float rRotate;
-    private float rRotateL;
-    private float rRotateR;
     private LineRenderer lineRenderer;
     private Vector3[] positions;
+    private List<Vector2> vertices = new List<Vector2>();
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +31,10 @@ public class CircleController : MonoBehaviour
         lineRenderer.startWidth = 0.2f; lineRenderer.endWidth = 0.2f;
 
         if (radiusConnection == false)
-            GenerateNGon(lineRenderer, sides);
+            GenerateNGon();
+
+        if (radiusConnection == true)
+            GenerateLRPolygon();
 
         lineRenderer.SetPositions(positions);
     }
@@ -43,7 +45,7 @@ public class CircleController : MonoBehaviour
         
     }
 
-    private void GenerateNGon(LineRenderer lr, int sides)
+    private void GenerateNGon()
     {
         positions = new Vector3[sides];
         lineRenderer.positionCount = positions.Length;
@@ -57,5 +59,33 @@ public class CircleController : MonoBehaviour
         }
     }
 
-    //private
+    private void GenerateLRPolygon()
+    {
+        vertices.Clear();
+
+        float RadRotateL = rotateL * Mathf.Deg2Rad;
+        float RadRotateR = rotateR * Mathf.Deg2Rad;
+    }
+
+    private Vector3 CalculateIntersection(float theta)
+    {
+        float radRotate = rotate * Mathf.Deg2Rad; //公式中的γ
+        float radTheta = theta * Mathf.Deg2Rad; //公式中的θ
+
+        //计算边索引k
+        float adjustedAngleL = (radTheta - radRotate + 2 * Mathf.PI) % (2 * Mathf.PI);
+        int k = Mathf.FloorToInt(adjustedAngleL * sides / (2 * Mathf.PI)) + 1;
+        k %= sides;
+
+        //计算公式的分母
+        float denominatorAngle = radTheta - radRotate - (2 * k - 1) * Mathf.PI / sides;
+        float denominator = Mathf.Cos(denominatorAngle);
+
+        //计算交点坐标
+        float scale = Mathf.Cos(Mathf.PI / sides) / denominator;
+        return new Vector3(
+            transform.position.x + radius * Mathf.Cos(radTheta) * scale,
+            transform.position.y + radius * Mathf.Sin(radTheta) * scale,
+            0);
+    }
 }
